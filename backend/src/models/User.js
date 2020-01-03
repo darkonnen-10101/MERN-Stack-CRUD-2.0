76c9d9b909362bcee1e2c563f9c6c8381b792b03
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-01-03 13:21:56 -0300

    ExpressJS REST API Mongoose

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-  username : {String, trim: true, unique: true},
+  username : {type: String, trim: true, unique: true},
 
 
 }, {
